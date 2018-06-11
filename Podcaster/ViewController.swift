0c523633fb6fd94768b1f36bf46bfcad17d3ad75
IvicaPetrsoric <ivica.petrsoric@gmail.com>
2018-06-11 22:26:02 +0200

    Initial Commit

---
+++ Podcaster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Podcaster
+//
+//  Created by Ivica Petrsoric on 11/06/2018.
+//  Copyright © 2018 Ivica Petrsoric. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
